fe51a38ddf5a6108d02f9224d6c0e04566772a42
Stenio Oliveira <steniooliv@gmail.com>
2021-05-20 18:00:50 -0300

    update controller

--- app/controllers/accountings_controller.rb
+++ app/controllers/accountings_controller.rb
@@ -1,9 +1,19 @@
 class AccountingsController < ApplicationController
+
+  before_action :set_accounting, only: :show
+
   def index
-    @accountings = Accounting.all.order(:name)
+    @accountings = Accounting.all
     render json: @accountings
   end
-
+  
   def show
+  end
+
+  private
+
+  def set_accounting
+    @accounting = Accounting.find(params[:id])
+    render json: @accounting
   end
 end
